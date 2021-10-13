dfca0a08beafe6b5a472202c098b85b00691e8f9
Fernando Herrera <fernando.herrera85@gmail.com>
2021-10-13 18:16:23 -0400

    adding semantic support

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,5 @@
-export * from './components/MyLabel';
+export { Button } from './stories/Button';
+export { Header } from './stories/Header';
+export { MyLabel } from './components/MyLabel';
+export { Page } from './stories/Page';
 
